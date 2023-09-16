736679f64aaa00422c1f6042964231981ad0a5e9
Moritz Schramm <moritz.schramm@gmx.de>
2023-09-17 00:07:05 +0200

    Add basic kanban board

--- src/rendering/controller/board.rendering.ts
+++ src/rendering/controller/board.rendering.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Param, Render } from '@nestjs/common';
 import { TicketService } from '../../ticket/ticket.service';
 import { ConfigService } from '@nestjs/config';
 
@@ -16,6 +16,34 @@ export class BoardRendering {
 
     return {
       projects
+    };
+  }
+
+  @Get('/:projectId/:boardName')
+  @Render('board/board')
+  async board(
+    @Param('projectId') projectId: string,
+    @Param('boardName') boardName: string
+  ) {
+    const project = this.configService
+      .get('projects')
+      .find((project) => project.id === projectId);
+
+    const board = project.boards.find((board) => board.name === boardName);
+
+    const tickets = await this.ticketService.search({
+      project: projectId
+    });
+
+    board.columns = board.columns.map((column) => ({
+      ...column,
+      tickets: tickets.filter((ticket) => column.status.includes(ticket.status))
+    }));
+
+    return {
+      project,
+      tickets,
+      board
     };
   }
 }
